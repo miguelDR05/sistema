66927fd61cd5f3ea47f91f1c6dd9ab5304e2f2a7
miguelDR05 <88801050+miguelDR05@users.noreply.github.com>
2022-08-26 09:09:54 -0500

    Update EmpleadoController.php
    Paginamos los registros a partir de 3 empleados

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -15,7 +15,7 @@ class EmpleadoController extends Controller
      */
     public function index()
     {
-        $datos['empleados'] = Empleado::paginate(1);
+        $datos['empleados'] = Empleado::paginate(3);
         
         return view('empleado.index', $datos);
     }
